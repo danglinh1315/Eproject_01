40b9e27ecd2cd31b3a066699c1f468566bb9fd7b
danglinh1315 <154239504+danglinh1315@users.noreply.github.com>
2023-12-28 22:26:54 +0700

    Update signUp.js

--- script/signUp.js
+++ script/signUp.js
@@ -21,11 +21,4 @@ function userSignup() {
     localStorage.setItem(user, password);
     alert('Registration successfully!');
 }
-function userLogin(){
-    var user = document.getElementById('user').value
-    var password = document.getElementById('password').value
-    var storedpassword = localStorage.getItem(user)
-    if(storedpassword==password){
-        alert('You sign in successfully!')
-    }
-}
+
