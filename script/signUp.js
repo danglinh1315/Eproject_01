e06f24049b100bcec56369b38931cec0c0817d33
danglinh1315 <154239504+danglinh1315@users.noreply.github.com>
2023-12-28 20:28:34 +0700

    Update signUp.js

--- script/signUp.js
+++ script/signUp.js
@@ -2,7 +2,7 @@ function userSignup(){
 
     var user = document.getElementById('username_user').value
     var password = document.getElementById('password_user').value
-    var repassword = document.getElementById('repassword_user').value
+    var repassword = document.getElementById('rpassword_user').value
     if(localStorage.getItem(user){
         alert('username already exists. Please choose another username.')
         return;}
