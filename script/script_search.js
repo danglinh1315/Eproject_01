c60abc59b3007d84b0fcb9cc0d39e760fd920940
danglinh1315 <154239504+danglinh1315@users.noreply.github.com>
2024-01-07 15:44:47 +0700

    V6

--- script/script_search.js
+++ script/script_search.js
@@ -4,12 +4,48 @@ var pagesData = [
     { id: 'home_1_2', hashtags: ['THE ENLIGHTEMENT AND GOLDEN AGE OF ALPINISM'], target: 'homepage.html' },
     { id: 'home_1_3', hashtags: ['EXPANSION AROUND THE WORLD'], target: 'homepage.html' },
     { id: 'home_1_4', hashtags: ['THE LAST FRONTIER: THE HIMALAYAS'], target: 'homepage.html' },
-    { id: 'technican', hashtags: ['TECHNICAN'], target: 'technican.html' },
-    { id: 'latestdevelopment', hashtags: ['LATEST DEVELOPMENT'], target: 'development.html' },
+    { id: 'home_2', hashtags: ['THE LAST FRONTIER: THE HIMALAYAS'], target: 'homepage.html' },   
+    { id: 'home_3', hashtags: ['TYPES'], target: 'homepage.html' },
+    { id: 'home_3_1', hashtags: ['ROCK CLIMBING'], target: 'homepage.html' },
+    { id: 'home_3_2', hashtags: ['ICE CLIMBING'], target: 'homepage.html' },
+    { id: 'home_3_3', hashtags: ['EXPEDITION CLIMBING'], target: 'homepage.html' },
+    { id: 'home_3_4', hashtags: ['SCRAMBLING'], target: 'homepage.html' },
+    { id: 'home_3_5', hashtags: ['SNOWSHOEING'], target: 'homepage.html' },    
+    { id: 'home_4', hashtags: ['RECORDS'], target: 'homepage.html' },
+    { id: 'home_5', hashtags: ['HAZARDS'], target: 'homepage.html' },
+
+
+    { id: 'technican', hashtags: ['TECHNICAN'], target: 'technican.html' },    
+    { id: 'technican_1', hashtags: ['TYPES OF CLIMBING'], target: 'technican.html' },
+    { id: 'technican_2', hashtags: ['WHAT CLIMBING GEAR DO YOU NEED?'], target: 'technican.html' },
+    { id: 'technican_3', hashtags: ['HOW TO LEARN ALPINE CLIMBING'], target: 'technican.html' },
+    { id: 'technican_4', hashtags: ['CONSTRUCTING SECURE ANCHORS'], target: 'technican.html' },
+    { id: 'technican_5', hashtags: ['HOW TO TIE OFF A BELAY'], target: 'technican.html' },
+    { id: 'technican_6', hashtags: ['ROCKFALL AND EDGES'], target: 'technican.html' },
+    
+
+    { id: 'latest', hashtags: ['LATEST DEVELOPMENT'], target: 'development.html' },
+    { id: 'latest_1', hashtags: ['TECHNOLOGY INTEGRATION'], target: 'development.html' },
+    { id: 'latest_2', hashtags: ['EQUIPMENT INNOVATIONS'], target: 'development.html' },
+    { id: 'latest_3', hashtags: ['SAFETY MEASURES'], target: 'development.html' },
+    { id: 'latest_4', hashtags: ['ENVIROMENTAL AWARENESS'], target: 'development.html' },
+    { id: 'latest_5', hashtags: ['COMMUNITY AND SOCIAL IMPACT'], target: 'development.html' },
+    { id: 'latest_6', hashtags: ['ADVENTURE TOURISM'], target: 'development.html' },
+    { id: 'latest_7', hashtags: ['MEDICAL AND FITNESS RESEARCH'], target: 'development.html' },
+
     { id: 'gallery', hashtags: ['GALLERY'], target: 'gallery.html' },
     { id: 'club', hashtags: ['CLUB'], target: 'club.html' },
-    { id: 'guideline', hashtags: ['GUIDELINES'], target: 'guidelines.html' }
-    // Thêm dữ liệu cho các trang khác nếu cần
+    { id: 'guideline', hashtags: ['GUIDELINES'], target: 'guidelines.html' },
+    { id: 'guideline_1', hashtags: ['PREPARING'], target: 'guidelines.html' },
+    { id: 'guideline_2', hashtags: ['RESEARCH AND PLANNING'], target: 'guidelines.html' },
+    { id: 'guideline_3', hashtags: ['NAVIGATION AND TECHNIQUES'], target: 'guidelines.html' },
+    { id: 'guideline_4', hashtags: ['SHELTERING AND HAZARDS'], target: 'guidelines.html' },
+    { id: 'guideline_5', hashtags: ['RECORDS AND SUCCESS STORIES'], target: 'guidelines.html' },
+    { id: 'guideline_6', hashtags: ['GGEOLOCATION AND SUMMIT'], target: 'guidelines.html' },
+    { id: 'guideline_7', hashtags: ['GALLERY OF ADVENTURES'], target: 'guidelines.html' },
+    { id: 'guideline_8', hashtags: ['GENERAL GUIDELINES'], target: 'guidelines.html' }
+    
+        // Thêm dữ liệu cho các trang khác nếu cần
 ];
 
 function search() {
